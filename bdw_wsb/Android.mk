c7308c1f363ed0bbe49dfa85bcf6d7ee708f14a5
Archana Patni <archana.patni@intel.com>
2012-04-02 19:45:43 +0530

    libsensors: Modified target to bdw_wsb in Android.mk
    This patch changes target devices to use bdw_wsb. As we will have two different
    targets - bdw_wsb and bdw_rvp
    
    Change-Id: I932e641c2ae04f6c9208dff6cb3928442b676848
    Signed-off-by: Archana Patni <archana.patni@intel.com>
    Reviewed-on: https://android.intel.com/166541
    Reviewed-by: Sesha, Subramony <subramony.sesha@intel.com>
    Tested-by: Sesha, Subramony <subramony.sesha@intel.com>
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>

--- bdw_wsb/Android.mk
+++ bdw_wsb/Android.mk
@@ -15,7 +15,7 @@
 # HAL module implemenation, not prelinked, and stored in
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 
-ifeq ($(TARGET_BOARD_PLATFORM),broadwell)
+ifeq ($(TARGET_PRODUCT_DEVICE),bdw_wsb)
 ifeq ($(BOARD_USE_PLATFORM_SENSOR_LIB),true)
 
 LOCAL_PATH := $(call my-dir)
